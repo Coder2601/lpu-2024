1884927c34d61f3d15fc1e765d0242fccb46ceb8
Divya Goyal <77169739+Coder2601@users.noreply.github.com>
2024-06-28 14:03:18 +0530

    Add files via upload

--- SocialMediaApp/FED/edit-post.js
+++ SocialMediaApp/FED/edit-post.js
@@ -1,13 +1,81 @@
 let postId = localStorage.getItem("editpostId");
+let token = localStorage.getItem("token");
+let postObj = {};
 
-async function handlePostData(){
-    let response = await fetch(`http://localhost:5000/socialMedia/posts/getPost/${postId}`,{
-        method:"GET",
-        headers:{'Content-type':'application/json'}
+async function handlePostData() {
+    if (!postId) {
+        alert("There is no post selected for editing.");
+        window.location.href = "view-posts.html";
+        return;
+    }
+
+    let response = await fetch(`http://localhost:5000/socialMedia/posts/getPost/${postId}`, {
+        method: "GET",
+        headers: { 
+            'Content-type': 'application/json',
+            'Authorization':`Basic ${token}`
+         }
+    })
+
+    let result = await response.json();
+    // console.log(result);
+
+    if (result.status) {
+        postObj = {...result.data};
+        document.getElementById("title").value = result.data.title;
+        document.getElementById("content").value = result.data.content;
+        document.getElementById("category").value = result.data.category;
+    }else{
+        alert(result.msg);
+        if(result.msg == "Error in finding the Post"){
+            window.location.href = "view-posts.html"
+        }else{
+            localStorage.removeItem("token");
+            localStorage.removeItem("editpostId");
+            window.location.href = "login.html"
+        }
+    }
+
+}
+
+function handleInput(event){
+    postObj[event.target.id] = event.target.value;
+}
+
+async function editPost(event){
+    event.preventDefault();
+    if (!postId) {
+        alert("There is no post selected for editing.");
+        window.location.href = "view-posts.html";
+        return;
+    }
+
+    let response = await fetch(`http://localhost:5000/socialMedia/posts/editPost/${postId}`,{
+        method:"PUT",
+        headers:{
+            'Content-type':'application/json',
+            'Authorization':`Basic ${token}`
+        },
+        body:JSON.stringify(postObj)
     })
 
     let result = await response.json();
     console.log(result);
 
-    
+    if(result.status){
+        alert(result.msg);
+        window.location.href = "view-posts.html";
+    }
+    else{
+        if(result.msg = "Unauthorised Access")
+        {
+            alert(result.msg);
+            localStorage.removeItem("token");
+            localStorage.removeItem("editpostId");
+            window.location.href = "login.html"
+        }else{
+            alert(result.msg);
+            window.location.href = "view-posts.html";
+        }
+    }
 }
