6fbe2295901bfb4b5c8f0d08e84c18353ffb9be9
Divya Goyal <77169739+Coder2601@users.noreply.github.com>
2024-06-28 11:11:22 +0530

    Add files via upload

--- SocialMediaApp/BED/routes/postRoutes.js
+++ SocialMediaApp/BED/routes/postRoutes.js
@@ -5,39 +5,54 @@ const postModel = require("../models/postModel");
 
 require("dotenv").config();
 
-function validateToken(req,res,next){
+function validateToken(req, res, next) {
     let authHeaders = req.headers.authorization;
 
     let token = authHeaders && authHeaders.split(" ")[1];
     try {
         let result = jwt.verify(token, process.env.ACCESS_SECRET_KEY);
         // console.log(result);
-        req.body = {data:{username:result.user_name, ...req.body}, status:true}
+        req.body = { data: { username: result.user_name, ...req.body }, status: true }
 
     } catch (error) {
         // console.log(error);
-        req.body = {msg:"Session Expired", status:false}
+        req.body = { msg: "Session Expired", status: false }
     }
     next();
 
 }
 
-postRoutes.post('/createPost',validateToken,async(req,res)=>{
+postRoutes.post('/createPost', validateToken, async (req, res) => {
 
-    if(!req.body.status){
-        res.json({msg:"Session Expired", status:false})
+    if (!req.body.status) {
+        res.json({ msg: "Session Expired", status: false })
     }
-    else{
+    else {
         let postData = req.body.data;
         console.log(postData);
 
-        const post = new postModel({...postData});
+        const post = new postModel({ ...postData });
         await post.save();
 
-        res.json({msg:"Saving the post data...", status:true})
+        res.json({ msg: "Saving the post data...", status: true })
     }
 })
 
+postRoutes.get('/author/:authorName', async (req, res) => {
+    let authorName = req.params.authorName;
+    
+    try {
+        let posts = await postModel.find({ author: authorName });
 
+        if (posts.length > 0) {
+            res.json({ data: posts, status: true })
+        } else {
+            res.json({ msg: "No Such Author Found, try another author", status: false })
+        }
+    } catch (error) {
+        res.json({msg:"Error is finding the author, Try another author", status: false})
+    }
+
+})
 
 module.exports = postRoutes;
